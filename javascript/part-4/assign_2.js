96254ce6cb11a5d8bfbd0d7e8642e37e5cb15b5d
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-03-16 19:44:52 +0530

    find no of digits in a number

---
+++ javascript/part-4/assign_2.js
@@ -0,0 +1,15 @@
+/* Qs2. Write a JS program to find the no of digits in a number.
+Example : if number = 287152, count = 6
+ */
+
+// ......................................................................
+
+const number = 287152;
+let copiedNumber = number;
+let count = 0;
+
+while (copiedNumber > 0) {
+  count++;
+  copiedNumber = Math.floor(copiedNumber / 10);
+}
+console.log(count);
