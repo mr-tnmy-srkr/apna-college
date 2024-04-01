74f29fe9a0f4999c0f5bceab1aa050026d6f79a8
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-04-01 17:21:03 +0530

    improved version added

--- javascript/part-11/newPromise.js
+++ javascript/part-11/newPromise.js
@@ -3,11 +3,34 @@ function saveToDb(data) {
     let internetSpeed = Math.floor(Math.random() * 10) + 1;
 
     if (internetSpeed > 4) {
-        resolve("Data saved successfully");
+      resolve("Data saved successfully");
     } else {
-        reject("weak internet speed");
+      reject("Weak internet speed");
     }
   });
 }
 
-console.log(saveToDb("Apna Clg"));
+/* saveToDb("Apna college")
+  .then((message) => {
+    console.log("resolved");
+    console.log(message); //Data saved successfully
+  })
+  .catch((error) => {
+    console.log("rejected"); //Weak internet speed
+    console.log(error);
+  });
+ */
+
+//   improved version
+
+saveToDb("Apna College")
+  .then(() => {
+    console.log("data1 saved successfully");
+    saveToDb("Hello World")
+    .then(() => {
+      console.log("data2 saved successfully");
+    });
+  })
+  .catch(() => {
+    console.log("rejected");
+  });
