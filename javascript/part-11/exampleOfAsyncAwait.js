66ff9f4bae6a1eebfd00b16793dd610cc086ae64
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-04-03 20:37:40 +0530

    text color change example by async await

---
+++ javascript/part-11/exampleOfAsyncAwait.js
@@ -0,0 +1,46 @@
+const h1 = document.getElementById("heading");
+
+const changeColor = (color, delay) => {
+  return new Promise((resolve, reject) => {
+    // Check if color is valid
+    if (typeof color !== "string" || !color.match(/^#[0-9A-F]{6}$/i)) {
+      reject("Invalid color format. Please provide a valid hexadecimal color.");
+      return; // Stop execution
+    }
+
+    setTimeout(() => {
+      h1.style.fontSize = "100px";
+      h1.style.color = color;
+      resolve("color changed");
+    }, delay);
+  });
+};
+
+/* changeColor("#FF0000", 1000)
+  .then((result) => {
+    console.log("color changed to red :", result);
+    return changeColor("#00FF00", 1000);
+  })
+  .then((result) => {
+    console.log("color changed to green :", result);
+    return changeColor("#0000FF", 1000);
+  })
+  .then((result) => {
+    console.log("color changed to blue :", result);
+  })
+  .catch((err) => {
+    console.log(err);
+  }); */
+
+async function demo() {
+  try {
+    await changeColor("#FF0000", 1000);
+    await changeColor("#00FF00", 1000);
+    await changeColor("#0000FF", 1000);
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(11); // promise reject holeo eta 11 ans asbe karon error handling kora ache code execution fail hobe na
+}
+
+demo();
