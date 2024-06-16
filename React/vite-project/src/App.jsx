d44096cad45ef1e902721d21670c8fd455597d65
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-06-16 20:01:10 +0530

    basic todo tasks adds

--- React/vite-project/src/App.jsx
+++ React/vite-project/src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([{ task: "sample tasks", id: uuidv4() }]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const addNewTasks = (e) => {
+    setNewTodo(e.target.value);
+  };
+  const updateTodoValue = () => {
+    setTodos([...todos, { task: newTodo, id: uuidv4() }]);
+    setNewTodo("");
+  };
+  const handleDeleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const handleUpperCase = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            task: todo.task.toUpperCase(),
+          };
+        } else {
+          return { ...todo };
+        }
+      })
+    );
+  };
+  const upperCaseAll = () => {
+    setTodos(
+      todos.map((todo) => {
+        return {
+          ...todo,
+          task: todo.task.toUpperCase(),
+        };
+      })
+    );
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        type="text"
+        name=""
+        id=""
+        value={newTodo}
+        onChange={addNewTasks}
+        placeholder="Add a tasks"
+      />
+      <br />
+      <br />
+      <br />
+      <button onClick={updateTodoValue}>Add a Tasks</button>
+      <br />
+      <br />
+      <br />
+      <hr />
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span>{todo.task}</span>
+            <span>
+              <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+            </span>
+            <span>
+              <button onClick={() => handleUpperCase(todo.id)}>
+                Uppder case
+              </button>
+            </span>
+          </li>
+        ))}
+      </ul>
+      <br />
+      <br />
+      <button onClick={upperCaseAll}>UpperCase-All</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
