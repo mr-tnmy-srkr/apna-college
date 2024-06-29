e2fe7d654015693ca190feddf4ad64da6634f507
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-06-29 18:42:10 +0530

    lottery component ready

--- React/lottery-game/src/Lottery.jsx
+++ React/lottery-game/src/Lottery.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import "./Lottery.css";
-import { genTicket } from "./Helper";
+import { genTicket, sum } from "./Helper";
 
 const Lottery = () => {
   let [ticket, setTicket] = useState(genTicket(3));
+let isWining = sum(ticket) === 15;
+
+let buyTicket = ()=>{
+setTicket(genTicket(3))
+}
+
   return (
     <div>
       <h1>Lottery Game!</h1>
@@ -12,6 +18,9 @@ const Lottery = () => {
         <span>{ticket[1]}</span>
         <span>{ticket[2]}</span>
       </div>
+      <br />
+      <button className="btn" onClick={buyTicket}>Buy Ticket</button>
+      {isWining ? <h2 className="won">Congratulation , you won!</h2> : <h2 className= "lose">Please try again later!</h2>}
     </div>
   );
 };
